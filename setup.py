48b8a1dc0a464f0e4191d459b2aa24ef0c2cb00c
voyz <voy1982@yahoo.co.uk>
2020-11-15 10:30:21 +0700

    release 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name='ibeam',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist"]),
-    version='0.1.0',
+    version='0.2.0',
     license='Apache-2.0',
     description='IBeam is an authentication and maintenance tool used for the Interactive Brokers Client Portal Web API Gateway.',
     long_description=long_description,
