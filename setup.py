a5b48486cfbd0fc2e49e21b6af34d240901dc1c7
voyz <voy1982@yahoo.co.uk>
2024-04-28 12:24:36 +0800

    switched over to reading requirements.txt instead of specifying install_requires manually. This caused too many annoyances, so whatevs

--- setup.py
+++ setup.py
@@ -7,9 +7,16 @@ import ibeam
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    parsed_requirements = f.read().splitlines()
+
+pakages_found = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist"])
+
+print(f'Found packages: {pakages_found}')
+
 setup(
     name='ibeam',
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist"]),
+    packages=pakages_found,
     version=ibeam.__version__,
     license='Apache-2.0',
     description='IBeam is an authentication and maintenance tool used for the Interactive Brokers Client Portal Web API Gateway.',
@@ -20,16 +27,7 @@ setup(
     url='https://github.com/Voyz/ibeam',
     keywords=['interactive brokers', 'algo trading',
               'algorithmic trading', 'data flow', 'quant', 'trading'],
-    install_requires=[
-        'selenium==3.*',
-        'cryptography==40.*',
-        'pyvirtualdisplay==3.*',
-        'apscheduler==3.*',
-        'psutil==5.*',
-        'requests==2.*',
-        'pillow==9.*'
-    ],
-
+    install_requires=parsed_requirements,
     classifiers=[
         'Development Status :: 4 - Beta',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
